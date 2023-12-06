f4179cce5c2f3d4d48c62f2bb295877deac26e69
larrymilson7 <136969130+larrymilson7@users.noreply.github.com>
2023-12-06 21:12:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,39 @@ def dashboard():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    webhook_message = json.loads(request.data)
+    try:
+        webhook_message = json.loads(request.data)
+        
+        if webhook_message['passphrase'] != config.WEBHOOK_PASSPHRASE:
+            return jsonify({
+                'code': 'error',
+                'message': 'Nice try buddy'
+            })
+        
+        price = webhook_message['strategy']['order_price']
+        symbol = webhook_message['ticker']
+        side = webhook_message['strategy']['order_action']
+        
+        quantity = 4  # Set the quantity to 4 contracts or any fixed value you prefer
+        
+        order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=price)
 
-    if webhook_message['passphrase'] != config.WEBHOOK_PASSPHRASE:
-        return jsonify({
-            'code': 'error',
-            'message': 'Nice try buddy'
-        })
+        if config.DISCORD_WEBHOOK_URL:
+            chat_message = {
+                "username": "strategyalert",
+                "avatar_url": "https://i.imgur.com/4M34hi2.png",
+                "content": f"TradingView strategy alert triggered: {quantity} {symbol} @ {price}"
+            }
+
+            requests.post(config.DISCORD_WEBHOOK_URL, json=chat_message)
+
+        return jsonify(webhook_message)
     
-    price = webhook_message['strategy']['order_price']
-    symbol = webhook_message['ticker']
-    side = webhook_message['strategy']['order_action']
-    
-    quantity = 4  # Set the quantity to 4 contracts or any fixed value you prefer
-    
-    order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=price)
-
-    if config.DISCORD_WEBHOOK_URL:
-        chat_message = {
-            "username": "strategyalert",
-            "avatar_url": "https://i.imgur.com/4M34hi2.png",
-            "content": f"TradingView strategy alert triggered: {quantity} {symbol} @ {price}"
-        }
-
-        requests.post(config.DISCORD_WEBHOOK_URL, json=chat_message)
-
-    return jsonify(webhook_message)
+    except json.decoder.JSONDecodeError as e:
+        # Print the error for debugging purposes
+        print("JSON decoding error:", e)
+        # Return a simple error response
+        return jsonify({'error': 'JSON decoding error'})
 
 if __name__ == "__main__":
     app.run()
