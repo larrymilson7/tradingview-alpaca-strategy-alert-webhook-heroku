e8bf7eea7cf3086ac5f95e0a8b0c6ec37c2f2fa4
larrymilson7 <136969130+larrymilson7@users.noreply.github.com>
2023-12-28 22:25:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url='https://paper-api.alpaca.markets')
 
+def close_positions(symbol, current_side):
+    positions = api.list_positions()
+    for position in positions:
+        if position.symbol == symbol:
+            if int(position.qty) > 0 and current_side == 'sell':
+                api.submit_order(symbol, abs(int(position.qty)), 'sell', 'market', 'gtc')
+            elif int(position.qty) < 0 and current_side == 'buy':
+                api.submit_order(symbol, abs(int(position.qty)), 'buy', 'market', 'gtc')
+
 @app.route('/')
 def dashboard():
     orders = api.list_orders()
-    
     return render_template('dashboard.html', alpaca_orders=orders)
 
 @app.route('/webhook', methods=['POST'])
@@ -22,22 +30,21 @@ def webhook():
             'message': 'nice try buddy'
         }
     
-    price = float(webhook_message['strategy']['order_price'])  # Convert to float if needed
-    rounded_price = round(price, 2)  # Round price to 2 decimal places
     quantity = webhook_message['strategy']['order_contracts']
     symbol = webhook_message['ticker']
     side = webhook_message['strategy']['order_action']
-    
-    order = api.submit_order(symbol, quantity, side, 'limit', 'gtc', limit_price=rounded_price)  # Use rounded_price here
 
-    # if a DISCORD URL is set in the config file, we will post to the discord webhook
+    # Close positions if needed before opening new ones
+    close_positions(symbol, side)
+
+    order = api.submit_order(symbol, quantity, side, 'market', 'gtc')
+
     if config.DISCORD_WEBHOOK_URL:
         chat_message = {
             "username": "strategyalert",
             "avatar_url": "https://i.imgur.com/4M34hi2.png",
-            "content": f"tradingview strategy alert triggered: {quantity} {symbol} @ {rounded_price}"  # Use rounded_price here
+            "content": f"tradingview strategy alert triggered: {quantity} {symbol} - Market Order"
         }
-
         requests.post(config.DISCORD_WEBHOOK_URL, json=chat_message)
 
     return webhook_message
